73150f0f2bbc988258810bb5e4d3a05c23d3d88b
Karan Visani <karanvisani719@gmail.com>
2026-09-22 16:10:48 +0530

    chore: initialize TypeScript Express project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express"
+
+const app = express()
+
+const PORT = 3000
+
+app.get("/health",(req,res)=>{
+    res.json({
+        success:true,
+        message:`Server is running on : ${PORT}`
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}/health`);
+    
+})
