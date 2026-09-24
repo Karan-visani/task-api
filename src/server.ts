d3f1da6750de14a57f6855592d8c59787a487833
Karan Visani <karanvisani719@gmail.com>
2026-09-24 16:21:55 +0530

    fix:error handling and status codes

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import { taskRouter } from "./routes/task.routes"
+import { errorHandler } from "./middleware/errorHandler"
 
 const app = express()
 app.use(express.json())
@@ -7,6 +8,8 @@ app.use(express.json())
 const PORT = 3000
 
 app.use("/tasks",taskRouter)
+
+app.use(errorHandler);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
